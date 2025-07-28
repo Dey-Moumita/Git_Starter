878ed347d24ab3e968f725d6ce1a6f59b85e7709
Moumita Dey <talktomoumitadey@gmail.com>
2025-07-28 18:15:18 +0530

    My first commit(Added a calculator class in src)

---
+++ src/edu/moumita/practise/Calculator.java
@@ -0,0 +1,16 @@
+package edu.moumita.practise;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number : ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("The result is : " + (num1 + num2));
+        System.out.println("The result is : " + (num1 - num2));
+    }
+}
