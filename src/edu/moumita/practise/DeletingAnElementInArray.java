23db17f1411745a5364fecd04c66bc447e23ffbd
Moumita Dey <talktomoumitadey@gmail.com>
2026-02-01 10:40:44 +0530

    Add a program to delete an element in array by index

---
+++ src/edu/moumita/practise/DeletingAnElementInArray.java
@@ -0,0 +1,27 @@
+package edu.moumita.practise;
+
+import java.util.Scanner;
+
+public class DeletingAnElementInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        for(int i = 0;i<array.length;i++) {
+            System.out.println("Enter the element no. : "+(i+1));
+            array[i] = scanner.nextInt();
+        }
+        System.out.println("Enter the index no. which you want to delete : ");
+        int index = scanner.nextInt();
+
+        for(int i = index;i<size-1;i++){
+            array[i] = array[i+1];
+        }
+
+
+        for(int i = 0;i<size-1;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
