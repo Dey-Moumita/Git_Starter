84dcd35d9e398db26fdb3d9c285c6a2a7d62f6ee
Moumita Dey <talktomoumitadey@gmail.com>
2026-02-07 17:36:51 +0530

    Update some statements

--- src/edu/moumita/practise/PatternPrinting.java
+++ src/edu/moumita/practise/PatternPrinting.java
@@ -2,11 +2,26 @@ package edu.moumita.practise;
 
 public class PatternPrinting {
     public static void main(String[] args) {
-        for(int i = 1;i<=4;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(i);
+//        for(int i = 1;i<=4;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print(i);
+//            }
+//            System.out.println();
+//        }
+
+
+                for(int i = 1;i<=4;i++){
+                    for(int spaces = 1;spaces<=4-i;spaces++){
+                        System.out.print(" ");
+                    }
+                    for(int spaces = 1;spaces<=2*i-1;spaces++){
+                        System.out.print("*");
+                    }
+                    for(int spaces = 1;spaces<=4-i;spaces++){
+                        System.out.print(" ");
+                    }
+                    System.out.println();
+                }
             }
-            System.out.println();
         }
-    }
-}
+
